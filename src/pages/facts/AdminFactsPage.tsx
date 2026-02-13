ae8a1acae9bbf06540b3c2df2053f4a76eeaab30
Anubhav <anubhav.psit.2009@gmail.com>
2026-02-14 01:03:19 +0530

    added

--- src/pages/facts/AdminFactsPage.tsx
+++ src/pages/facts/AdminFactsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://coachify.local/api/v1';
